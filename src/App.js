8551cb8f677f895112a84bb866fcd2a5bf5d65fe
Nicholas Tey <jian000209@gmail.com>
2022-02-20 14:37:19 +0800

    publish

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
 import Alert from "@mui/material/Alert";
-import * as React from 'react';
+import * as React from "react";
 import { useState, useEffect } from "react";
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Index from "./containers/Index";
 import Home_cn from "./containers/index_cn";
 import Contact from "./containers/English/Contact";
 import Contact_cn from "./containers/Mandarin/Contact_cn";
 
 function App() {
+  const [isDev, setIsDev] = useState(false);
   return (
     <BrowserRouter>
-    <div className="App">
+      {isDev ? (
+        <div className="App">
+          <Switch>
+            <Route path="/introduce-portfolio/" component={Index} exact />
+            <Route path="/introduce-portfolio/Home_cn" component={Home_cn} />
+            <Route path="/introduce-portfolio/Contact" component={Contact} />
+            <Route
+              path="/introduce-portfolio/Contact_cn"
+              component={Contact_cn}
+            />
+          </Switch>
+        </div>
+      ) : (
+        <div className="App">
+          <Switch>
+            <Route path="https://jian0209.github.io/introduce-portfolio/" component={Index} exact />
+            <Route path="https://jian0209.github.io/introduce-portfolio/Home_cn" component={Home_cn} />
+            <Route path="https://jian0209.github.io/introduce-portfolio/Contact" component={Contact} />
+            <Route
+              path="https://jian0209.github.io/introduce-portfolio/Contact_cn"
+              component={Contact_cn}
+            />
+          </Switch>
+        </div>
+      )}
+      {/* <div className="App">
       <Switch>
         <Route path="/introduce-portfolio/" component={Index} exact />
         <Route path="/introduce-portfolio/Home_cn" component={Home_cn} />
         <Route path="/introduce-portfolio/Contact" component={Contact} />
         <Route path="/introduce-portfolio/Contact_cn" component={Contact_cn} />
       </Switch>
-    </div>
+    </div> */}
     </BrowserRouter>
   );
 }
